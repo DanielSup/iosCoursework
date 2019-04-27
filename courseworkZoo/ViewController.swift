2fab4ab6760ee9ef96167a51de34022eb0ab883a
Daniel Šup <danielsup@daniels-mac.local>
2019-04-27 12:13:42 -0700

    Initial Commit

---
+++ courseworkZoo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  courseworkZoo
+//
+//  Created by Daniel Šup on 27/04/2019.
+//  Copyright © 2019 Daniel Šup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
